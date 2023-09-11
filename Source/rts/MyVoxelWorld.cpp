4ecf1dcf2c0525d6dcec278208b59df53c3d51df
Zach Rizzo <85094894+zachrizzo@users.noreply.github.com>
2023-09-11 04:03:07 +0200

    greedy meshing
    the voxel engine now uses greedy meshing you can render 8,000,000 voxels with out a frame drop

--- Source/rts/MyVoxelWorld.cpp
+++ Source/rts/MyVoxelWorld.cpp
@@ -7,35 +7,40 @@
 #include "NoiseUtility.h"
 #include "MyVoxel.h"
 
-AMyVoxelWorld::AMyVoxelWorld()
-{
+AMyVoxelWorld::AMyVoxelWorld() {
     // Set this actor to call Tick() every frame
     PrimaryActorTick.bCanEverTick = true;
+    GlobalVertices.Empty();
+    GlobalTriangles.Empty();
     
-
     // Default grid size and voxel size
-    GridSize = FVector(20, 20, 20);
+    GridSize = FVector(200, 200,200);
     VoxelSize = 100.0f;
     VoxelArrayWidth = GridSize.X;
     VoxelArrayHeight = GridSize.Y;
     VoxelArrayDepth = GridSize.Z;
+    
+    // Initialize Greedy Procedural Mesh
+    GreedyProceduralMesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("GreedyGeneratedMesh"));
+    RootComponent = GreedyProceduralMesh;
+    
+    // Initialize VoxelArray and visibilityFlags3D 3D grid
     VoxelArray.SetNum(VoxelArrayWidth);
+    visibilityFlags3D.SetNum(VoxelArrayWidth);
     for (int x = 0; x < VoxelArrayWidth; ++x) {
         VoxelArray[x].SetNum(VoxelArrayHeight);
+        visibilityFlags3D[x].SetNum(VoxelArrayHeight);
         for (int y = 0; y < VoxelArrayHeight; ++y) {
             VoxelArray[x][y].SetNum(VoxelArrayDepth);
-        }
-        
-    }
-    visibilityFlags3D.SetNum(VoxelArrayWidth);
-        for (int x = 0; x < VoxelArrayWidth; ++x) {
-            visibilityFlags3D[x].SetNum(VoxelArrayHeight);
-            for (int y = 0; y < VoxelArrayHeight; ++y) {
-                visibilityFlags3D[x][y].SetNum(VoxelArrayDepth);
+            visibilityFlags3D[x][y].SetNum(VoxelArrayDepth);
+            for (int z = 0; z < VoxelArrayDepth; ++z) {
+                VoxelArray[x][y][z] = 0;
+                visibilityFlags3D[x][y][z] = TArray<bool>();  // Initialize with empty TArray
             }
         }
-
+    }
 }
+
 
 
 TArray<bool> AMyVoxelWorld::CalculateVisibilityFlags(int x, int y, int z) {
@@ -44,63 +49,32 @@ TArray<bool> AMyVoxelWorld::CalculateVisibilityFlags(int x, int y, int z) {
     
     if (x >= 0 && x < VoxelArrayWidth && y >= 0 && y < VoxelArrayHeight && z >= 0 && z < VoxelArrayDepth) {
         if (VoxelArray[x][y][z] == 1) {
-          
+            // Initialize all flags to true initially
+            for (int i = 0; i < 6; ++i) {
+                visibilityFlags[i] = true;
+            }
+
+            // Check all six directions and update the visibility flags
+            if (x - 1 >= 0 && VoxelArray[x - 1][y][z] == 1) visibilityFlags[0] = false;
+            if (x + 1 < VoxelArrayWidth && VoxelArray[x + 1][y][z] == 1) visibilityFlags[1] = false;
+            if (y - 1 >= 0 && VoxelArray[x][y - 1][z] == 1) visibilityFlags[2] = false;
+            if (y + 1 < VoxelArrayHeight && VoxelArray[x][y + 1][z] == 1) visibilityFlags[3] = false;
+            if (z - 1 >= 0 && VoxelArray[x][y][z - 1] == 1) visibilityFlags[4] = false;
+            if (z + 1 < VoxelArrayDepth && VoxelArray[x][y][z + 1] == 1) visibilityFlags[5] = false;
             
-            if (y + 1 < VoxelArrayHeight) {  // Check that we're not out of bounds
-                if (VoxelArray[x][y + 1][z] == 1) {  // There is a voxel immediately behind
-                    visibilityFlags[3] = false;
-                    UE_LOG(LogTemp, Warning, TEXT("0"));
-                
-                } else {
-                    visibilityFlags[3] = true;
-                }
-            } else {
-                // Handle the case where y + 1 is out of bounds. Here we assume no voxel is behind.
-                visibilityFlags[3] = true;
+            // Log the visibility flags for debugging
+            FString VisFlagsString;
+            for (bool flag : visibilityFlags) {
+                VisFlagsString.Append(flag ? TEXT("1, ") : TEXT("0, "));
             }
-
-            // Front
-            if (y - 1 >= 0 && VoxelArray[x][y - 1][z] == 1) {
-                visibilityFlags[2] = false;
-            } else {
-                visibilityFlags[2] = true;
-            }
-
-        
-            // Right (in negative X-direction)
-            if (x - 1 >= 0 && VoxelArray[x - 1][y][z] == 1) {
-                visibilityFlags[1] = false;
-            } else {
-                visibilityFlags[1] = true;
-            }
-
-
-
-            // Left
-            if (x + 1 < VoxelArrayWidth && VoxelArray[x + 1][y][z] == 1) {
-                visibilityFlags[0] = false;
-            } else {
-                visibilityFlags[0] = true;
-            }
-
-            // Top (in positive Z-direction)
-            if (z + 1 < VoxelArrayDepth && VoxelArray[x][y][z + 1] == 1) {
-                visibilityFlags[4] = false;
-            } else {
-                visibilityFlags[4] = true;
-            }
-
-            // Bottom (in negative Z-direction)
-            if (z - 1 >= 0 && VoxelArray[x][y][z - 1] == 1) {
-                visibilityFlags[5] = false;
-            } else {
-                visibilityFlags[5] = true;
-            }
+            UE_LOG(LogTemp, Warning, TEXT("Voxel at (%d, %d, %d) - Visibility Flags: %s"), x, y, z, *VisFlagsString);
         }
     }
-
+    
     return visibilityFlags;
 }
+
+
 
 
 
@@ -109,7 +83,7 @@ TArray<bool> AMyVoxelWorld::CalculateVisibilityFlags(int x, int y, int z) {
 void AMyVoxelWorld::OnConstruction(const FTransform& Transform)
 {
     Super::OnConstruction(Transform);
-    DestroyPreviousVoxels();
+//    DestroyPreviousVoxels();
     InitializeAsyncTasks();
     
 //    // Generate the greedy meshes for the entire grid
@@ -155,44 +129,45 @@ void AMyVoxelWorld::GenerateNewVoxels()
         }
     }
 
-    // Then, spawn the voxels and calculate visibility
-    for (int x = 0; x < GridSize.X; ++x)
-    {
-        for (int y = 0; y < GridSize.Y; ++y)
-        {
-            
-            PopulateVoxelArray(x, y);
-//            for (int z = 0; z < GridSize.Z; ++z)
-//            {
-//                if (VoxelArray[x][y][z] == 1)
-//                {
-//                    SpawnVoxel(x, y, z, VoxelFolderName);
-//                }
-//            }
+//    // Then, spawn the voxels and calculate visibility
+//    for (int x = 0; x < GridSize.X; ++x)
+//    {
+//        for (int y = 0; y < GridSize.Y; ++y)
+//        {
+//
+//            PopulateVoxelArray(x, y);
+////            for (int z = 0; z < GridSize.Z; ++z)
+////            {
+////                if (VoxelArray[x][y][z] == 1)
+////                {
+////                    SpawnVoxel(x, y, z, VoxelFolderName);
+////                }
+////            }
+//        }
+//
+//    }
+    if (GreedyMeshActor && GreedyMeshActor->IsValidLowLevel() && GreedyMeshActor->GetWorld()) {
+            GreedyMeshActor->Destroy();
         }
-     
-    }
     GenerateGreedyMeshes();
 }
 
 
-void AMyVoxelWorld::PopulateVoxelArray(int x, int y)
-{
+void AMyVoxelWorld::PopulateVoxelArray(int x, int y) {
     float height = UNoiseUtility::PerlinNoise(x * 0.1f, y * 0.1f) * 10;
     int maxHeight = FMath::RoundToInt(height);
 
-    for (int z = 0; z < GridSize.Z; ++z)
-    {
-        if (z <= maxHeight && x < VoxelArrayWidth && y < VoxelArrayHeight && z < VoxelArrayDepth)
-        {
+    for (int z = 0; z < GridSize.Z; ++z) {
+        if (z <= maxHeight && x < VoxelArrayWidth && y < VoxelArrayHeight && z < VoxelArrayDepth) {
             VoxelArray[x][y][z] = 1;
-        }
-        else if (x < VoxelArrayWidth && y < VoxelArrayHeight && z < VoxelArrayDepth)
-        {
+            visibilityFlags3D[x][y][z] = CalculateVisibilityFlags(x, y, z);
+        } else if (x < VoxelArrayWidth && y < VoxelArrayHeight && z < VoxelArrayDepth) {
             VoxelArray[x][y][z] = 0;
+            visibilityFlags3D[x][y][z].Init(false, 6);  // Initialize with 6 false values
         }
     }
 }
+
 
 
 void AMyVoxelWorld::GenerateColumn(int x, int y, const FName& VoxelFolderName)
@@ -285,14 +260,20 @@ void AMyVoxelWorld::Tick(float DeltaTime)
 
 void AMyVoxelWorld::GenerateGreedyMeshes()
 {
+    // Clear the existing vertices and triangles
+    GlobalVertices.Empty();
+    GlobalTriangles.Empty();
+    
+    // Destroy the previous mesh if it exists
+    if (GreedyMeshActor && GreedyMeshActor->IsValidLowLevel() && GreedyMeshActor->GetWorld()) {
+        GreedyMeshActor->Destroy();
+    }
+
     // Create a single AMyVoxel actor for the entire grid
     FVector Position(0, 0, 0);
-    AMyVoxel* VoxelActor = GetWorld()->SpawnActor<AMyVoxel>(Position, FRotator::ZeroRotator);
+    GreedyMeshActor = GetWorld()->SpawnActor<AMyVoxel>(Position, FRotator::ZeroRotator);
 
-    // Generate the greedy mesh for the entire grid and set it on the AMyVoxel actor
-    TArray<FVector> Vertices;
-    TArray<int32> Triangles;
-
+    // Generate the greedy mesh for the entire grid
     for (int z = 0; z < GridSize.Z; ++z)
     {
         TArray<bool> mask;
@@ -308,14 +289,18 @@ void AMyVoxelWorld::GenerateGreedyMeshes()
         }
 
         // Generate the mesh for this layer
-        GenerateMeshFromMask(mask, z, Vertices, Triangles);
+        GenerateMeshFromMask(mask, z, GlobalVertices, GlobalTriangles);
     }
 
     // Finally, update the single VoxelActor with the complete mesh
-    VoxelActor->SetGreedyMesh(Vertices, Triangles);
+    SetGreedyMesh(GlobalVertices, GlobalTriangles);}
+
+
+void AMyVoxelWorld::SetGreedyMesh(const TArray<FVector>& NewVertices, const TArray<int32>& NewTriangles)
+{
+    // Update the mesh using the new greedy vertices and triangles
+    GreedyProceduralMesh->CreateMeshSection_LinearColor(0, NewVertices, NewTriangles, TArray<FVector>(), TArray<FVector2D>(), TArray<FLinearColor>(), TArray<FProcMeshTangent>(), true);
 }
-
-
 
 
 bool AMyVoxelWorld::CanMerge(const TArray<bool>& mask, int x1, int y1, int x2, int y2, int width) {
@@ -329,69 +314,65 @@ void AMyVoxelWorld::GenerateMeshFromMask(const TArray<bool>& mask, int z, TArray
     TArray<bool> maskCopy = mask;
 
     for (int y = 0; y < GridSize.Y; ++y) {
-        int x = 0;
-        while (x < GridSize.X) {
-            if (mask[x + y * GridSize.X]) {
-                // We found a visible voxel. Try to extend it as far as possible in the X direction.
-                int width = 1;
-                while (x + width < GridSize.X && CanMerge(mask, x, y, x + width, y, GridSize.X)) {
-                    ++width;
+        for (int x = 0; x < GridSize.X; ++x) {
+            TArray<bool> visibilityFlags = visibilityFlags3D[x][y][z];
+            
+            // Check for a visible voxel at the current coordinates
+            if (maskCopy[x + y * GridSize.X] && visibilityFlags.Contains(true)) {
+                int width = 1, height = 1;
+                
+                // Check for expandability in x and y directions
+                for (width = 1; x + width < GridSize.X; ++width) {
+                    if (!CanMerge(maskCopy, x, y, x + width, y, GridSize.X) ||
+                        !visibilityFlags3D[x + width][y][z].Contains(true)) {
+                        break;
+                    }
                 }
-
-                // We've found a line of width "width" starting from (x, y). Now try to extend it in the Y direction.
-                int height = 1;
-                bool continueMerging = true;
-                while (y + height < GridSize.Y && continueMerging) {
+                
+                for (height = 1; y + height < GridSize.Y; ++height) {
+                    bool canMerge = true;
                     for (int dx = 0; dx < width; ++dx) {
-                        if (!CanMerge(mask, x + dx, y, x + dx, y + height, GridSize.X)) {
-                            continueMerging = false;
+                        if (!CanMerge(maskCopy, x + dx, y, x + dx, y + height, GridSize.X) ||
+                            !visibilityFlags3D[x + dx][y + height][z].Contains(true)) {
+                            canMerge = false;
                             break;
                         }
                     }
-                    if (continueMerging) {
-                        ++height;
-                    }
+                    if (!canMerge) break;
                 }
 
-                // Create the quad
+                // Create the quad vertices
                 FVector v0(x, y, z);
                 FVector v1(x + width, y, z);
                 FVector v2(x + width, y + height, z);
                 FVector v3(x, y + height, z);
-
-                // Calculate the additional vertices based on your specific requirements
-                FVector v4(x, y, z + 1);  // These are placeholders
-                FVector v5(x + width, y, z + 1);  // Update as needed
+                FVector v4(x, y, z + 1);
+                FVector v5(x + width, y, z + 1);
                 FVector v6(x + width, y + height, z + 1);
                 FVector v7(x, y + height, z + 1);
+
+                // Loop over each voxel within the rectangle to add its visible faces
+                for (int dx = 0; dx < width; ++dx) {
+                    for (int dy = 0; dy < height; ++dy) {
+                        TArray<bool> currentVisibilityFlags = visibilityFlags3D[x + dx][y + dy][z];
+                        AddVisibleFaces(v0, v1, v2, v3, v4, v5, v6, v7, currentVisibilityFlags, Vertices, Triangles);
+                    }
+                }
                 
-                int BaseVertexIndex = Vertices.Num();
-                
-                Vertices.Add(v0);
-                Vertices.Add(v1);
-                Vertices.Add(v2);
-                Vertices.Add(v3);
-                
-                AddVisibleFaces(v0, v1, v2, v3, v4, v5, v6, v7, CalculateVisibilityFlags(x, y, z), Vertices, Triangles);
-
-
-
-
-                // Update the mask to indicate that these voxels have been processed
+                // Update maskCopy to skip processed voxels
                 for (int dx = 0; dx < width; ++dx) {
                     for (int dy = 0; dy < height; ++dy) {
                         maskCopy[x + dx + (y + dy) * GridSize.X] = false;
                     }
                 }
 
-                // Move the cursor
-                x += width;
-            } else {
-                ++x;
+                x += width - 1; // Skip the voxels that were just processed
             }
         }
     }
 }
+
+
 
 
 void AMyVoxelWorld::AddVisibleFaces(
@@ -400,31 +381,21 @@ void AMyVoxelWorld::AddVisibleFaces(
     const TArray<bool>& visibilityFlags,
     TArray<FVector>& Vertices, TArray<int32>& Triangles)
 {
-    if (visibilityFlags[0]) {
-   
-        AddFace(v2, v1, v5, v6, Vertices,Triangles);   // Left
-    }
-    if (visibilityFlags[1]) {
-        AddFace(v0, v3, v7,v4 ,Vertices,Triangles);    // Right
-    }
-    if (visibilityFlags[2]) {
-        AddFace(v1, v0, v4, v5,Vertices,Triangles);      // Front
-    }
-    if (visibilityFlags[3]) {
-        AddFace(v6, v7, v3, v2,Vertices,Triangles);    //Back
-    }
-    if (visibilityFlags[4]) {
-        AddFace(v7, v6, v5, v4,Vertices,Triangles);   // Top face
-    }
-    if (visibilityFlags[5]) {
-        AddFace(v0, v1, v2, v3,Vertices,Triangles);    // Bottom
-    }
-    
+    // Note: Assuming visibilityFlags are in order: [Left, Right, Front, Back, Top, Bottom]
+    if (visibilityFlags[1]) AddFace(v2, v1, v5, v6, Vertices, Triangles);   // right
+    if (visibilityFlags[0]) AddFace(v0, v3, v7, v4, Vertices, Triangles);   // left
+    if (visibilityFlags[2]) AddFace(v1, v0, v4, v5, Vertices, Triangles);   // Front
+    if (visibilityFlags[3]) AddFace(v6, v7, v3, v2, Vertices, Triangles);   // Back
+    if (visibilityFlags[5]) AddFace(v7, v6, v5, v4, Vertices, Triangles);   // bottom
+    if (visibilityFlags[4]) AddFace(v0, v1, v2, v3, Vertices, Triangles);   // top
 }
-
-void AMyVoxelWorld::AddFace(const FVector& v0, const FVector& v1, const FVector& v2, const FVector& v3,
-                            TArray<FVector>& Vertices, TArray<int32>& Triangles)
+void AMyVoxelWorld::AddFace(const FVector& v0, const FVector& v1, const FVector& v2, const FVector& v3, TArray<FVector>& Vertices, TArray<int32>& Triangles)
 {
+    
+    //this function creats a face using the input vertices and adds it to the mesh
+        
+    
+    
     int32 BaseVertexIndex = Vertices.Num();
         
     // Define vertices based on input vertices
@@ -442,4 +413,17 @@ void AMyVoxelWorld::AddFace(const FVector& v0, const FVector& v1, const FVector&
     Triangles.Add(BaseVertexIndex + 2);
     Triangles.Add(BaseVertexIndex + 3);
     Triangles.Add(BaseVertexIndex);
+}
+
+void AMyVoxelWorld::DestroyVoxel(int x, int y, int z) {
+    // Validate the input coordinates
+    if (x < 0 || x >= GridSize.X || y < 0 || y >= GridSize.Y || z < 0 || z >= GridSize.Z) {
+        return;
+    }
+
+    // Destroy the voxel in the internal representation
+    VoxelArray[x][y][z] = 0;
+
+    // Regenerate the greedy mesh
+    GenerateGreedyMeshes();
 }
--- Source/rts/MyVoxelWorld.h
+++ Source/rts/MyVoxelWorld.h
@@ -4,6 +4,7 @@
 #include "GameFramework/Actor.h"
 #include "Async/Async.h"
 #include "MyVoxel.h"
+#include "ProceduralMeshComponent.h"
 #include "MyVoxelWorld.generated.h"
 
 UCLASS()
@@ -12,28 +13,42 @@ class RTS_API AMyVoxelWorld : public AActor
     GENERATED_BODY()
 
 public:
-    // Sets default values for this actor's properties
+    // Constructor
     AMyVoxelWorld();
 
     // Variables
     UPROPERTY(EditAnywhere)
     FVector GridSize;
+
     UPROPERTY(EditAnywhere)
     float VoxelSize;
 
     UPROPERTY(EditAnywhere, Category = "Mesh")
     TArray<UStaticMesh*> VoxelMeshes;
+    
+    UPROPERTY()
+        UProceduralMeshComponent* GreedyProceduralMesh;
 
-    int VoxelArrayWidth; // Fixed typo: VoxelArryWidth -> VoxelArrayWidth
-    int VoxelArrayHeight; // Fixed typo: VoxelArryHeight -> VoxelArrayHeight
-    int VoxelArrayDepth; // Fixed typo: VoxelArryDepth -> VoxelArrayDepth
-    TArray<TArray<TArray<bool>>> visibilityFlags3D;  // 3D array of visibility flags
+    int VoxelArrayWidth;
+    int VoxelArrayHeight;
+    int VoxelArrayDepth;
+
+    TArray<TArray<TArray<TArray<bool>>>> visibilityFlags3D;
     TArray<TArray<TArray<int>>> VoxelArray;
     TArray<AMyVoxel*> SpawnedVoxels;
+  
+
+
+    AActor* GreedyMeshActor;  // or the actual type of GreedyMeshActor
+    UProceduralMeshComponent* ProceduralMesh;  // or the actual type of ProceduralMesh
+
+    TArray<FVector> GlobalVertices;
+    TArray<int32> GlobalTriangles;
+    TArray<FVector> GreedyVertices;
+    TArray<int32> GreedyTriangles;
 
     // Methods
     virtual void OnConstruction(const FTransform& Transform) override;
-
     void InitializeAsyncTasks();
     void DestroyPreviousVoxels();
     void GenerateNewVoxels();
@@ -41,29 +56,26 @@ public:
     void SpawnVoxel(int x, int y, int z, const FName& VoxelFolderName);
     void PopulateVoxelArray(int x, int y);
     bool CanMerge(const TArray<bool>& mask, int x1, int y1, int x2, int y2, int width);
-    void AddFace(const FVector& v0, const FVector& v1, const FVector& v2, const FVector& v3,
-                     TArray<FVector>& Vertices, TArray<int32>& Triangles);
-
-
-    // Fixed function signature
+    void AddFace(const FVector& v0, const FVector& v1, const FVector& v2, const FVector& v3, TArray<FVector>& Vertices, TArray<int32>& Triangles);
+    void SetGreedyMesh(const TArray<FVector>& NewVertices, const TArray<int32>& NewTriangles);
     void AddVisibleFaces(
         const FVector& v0, const FVector& v1, const FVector& v2, const FVector& v3,
         const FVector& v4, const FVector& v5, const FVector& v6, const FVector& v7,
         const TArray<bool>& visibilityFlags,
         TArray<FVector>& Vertices, TArray<int32>& Triangles
     );
-    
     void DestroyVoxel(FVector WorldPosition);
     TArray<bool> CalculateVisibilityFlags(int x, int y, int z);
     void GenerateGreedyMeshes();
 
 protected:
-    // Methods
     virtual void BeginPlay() override;
 
 public:
-    // Methods
     virtual void Tick(float DeltaTime) override;
     void DestroySpawnedVoxels();
     void GenerateMeshFromMask(const TArray<bool>& mask, int z, TArray<FVector>& Vertices, TArray<int32>& Triangles);
+
+    UFUNCTION(BlueprintCallable, Category = "VoxelEngine")
+    void DestroyVoxel(int x, int y, int z);
 };
